cdad39789ff4d4f7affb2a734ab3917c54615c0f
Jay Medina <jaymedina@Jays-MacBook-Pro.local>
2023-01-03 09:22:08 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+/*
+var interval1 = setInterval(function () {
+  let div = document.getElementById("circle");
+  div.style.top = div.offsetTop + 2 + "px";
+  div.style.left = div.offsetLeft + 2 + "px";
+}, 1000);
+var interval2 = setInterval(function () {
+  let div = document.getElementById("diamond");
+  div.style.top = div.offsetTop + -2 + "px";
+  div.style.left = div.offsetLeft + 2 + "px";
+}, 1000);
+
+var interval3 = setInterval(function () {
+  let div = document.getElementById("cat");
+  div.style.top = div.offsetTop + -2 + "px";
+}, 1000);
+*/
